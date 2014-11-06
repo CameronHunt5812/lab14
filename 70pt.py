8d049c214a3ae3c346bdb69fa9f86a00c45e7fa2
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-11-06 15:07:16 -0600

    lab 14 70 point

--- 70pt.py
+++ 70pt.py
@@ -45,8 +45,14 @@ class MyApp:
                 # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
+		global player
+		drawpad.move(player,0,-10)
+		global target
                 x1,y1,x2,y2 = drawpad.coords(player)
 		global targetx1, targety1, targetx2, targety2
+		
+		if (x1 > targetx1) and (x2 < targetx2) and (y1 > targety1) and (y2 < targety2):
+		    drawpad.itemconfig(target,fill="red")
 
 
 		# Ensure that we are doing our collision detection
